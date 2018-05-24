3b7ca6b3b1988dbc8f446826ea0f75af5832a161
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2018-05-24 19:18:14 +0900

    refactor api.py

--- app/src/api.py
+++ app/src/api.py
@@ -13,17 +13,12 @@ import requests
 logger = getLogger(__name__)
 
 ORION_PATH = '/v1/updateContext'
-ORION_HEADER = {
-    'Fiware-Service': 'demo1',
-    'Fiware-Servicepath': '/',
-    'Content-Type': 'application/json',
-}
 ORION_PAYLOAD_TEMPLATE = {
     'contextElements': [
         {
-            'id': 'turtlesim',
+            'id': '',
             'isPattern': False,
-            'type': 'demo1',
+            'type': '',
             'attributes': [
                 {
                     'name': 'move',
@@ -42,6 +37,19 @@ else:
     ENDPOINT = urljoin('http://orion:1026', ORION_PATH)
 
 
+def send_request_to_orion(value):
+    headers = dict()
+    headers['Fiware-Service'] = os.environ.get('FIWARE_SERVICE', 'demo1')
+    headers['Fiware-Servicepath'] = os.environ.get('FIWARE_SERVICEPATH', '/')
+    headers['Content-Type'] = 'application/json'
+
+    data = copy.deepcopy(ORION_PAYLOAD_TEMPLATE)
+    data['contextElements'][0]['id'] = os.environ.get('ROBOT_ID', '')
+    data['contextElements'][0]['type'] = os.environ.get('ROBOT_TYPE', '')
+    data['contextElements'][0]['attributes'][0]['value'] = value
+    requests.post(ENDPOINT, headers=headers, data=json.dumps(data))
+
+
 class GamepadAPI(MethodView):
     NAME = 'gamepad'
 
@@ -52,9 +60,7 @@ class GamepadAPI(MethodView):
         for data in json.loads(payload)['data']:
             value = data['button']['value'].strip()
             if len(value) != 0:
-                data = copy.deepcopy(ORION_PAYLOAD_TEMPLATE)
-                data['contextElements'][0]['attributes'][0]['value'] = value
-                requests.post(ENDPOINT, headers=ORION_HEADER, data=json.dumps(data))
+                send_request_to_orion(value)
 
         return jsonify({'result': 'ok'})
 
@@ -69,9 +75,7 @@ class WebAPI(MethodView):
         if 'move' in request.form:
             value = request.form['move'].strip()
             if len(value) != 0:
-                data = copy.deepcopy(ORION_PAYLOAD_TEMPLATE)
-                data['contextElements'][0]['attributes'][0]['value'] = value
-                requests.post(ENDPOINT, headers=ORION_HEADER, data=json.dumps(data))
+                send_request_to_orion(value)
 
         if 'PREFIX' in os.environ:
             redirect_url = os.path.join('/', os.environ['PREFIX'], *url_for(WebAPI.NAME).split(os.sep)[1:])
