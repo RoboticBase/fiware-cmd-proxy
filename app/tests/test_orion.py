c40d33f587ba4b7376a1264f9212b46891474821
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2019-02-15 17:42:30 +0900

    edit tests

--- app/tests/test_orion.py
+++ app/tests/test_orion.py
@@ -12,28 +12,28 @@ from src import const
 class TestSendRequestToOrion:
 
     @pytest.mark.parametrize('v', [None, '', ' ', 'test value', '   test value 2   '])
-    def test_success_no_env(self, monkeypatch, endpoint, mocked_post, v):
-        mocked_post(monkeypatch, endpoint, '', '', '', '', v)
+    def test_success_no_env(self, monkeypatch, endpoint, mocked_patch, v):
+        mocked_patch(monkeypatch, endpoint, '', '', '', '', v)
         send_request_to_orion(endpoint, v)
 
     @pytest.mark.parametrize('fs', ['', ' ', 'test service'])
     @pytest.mark.parametrize('fsp', ['', ' ', 'test servicepath'])
-    @pytest.mark.parametrize('ri', ['', ' ', 'test robot id'])
-    @pytest.mark.parametrize('rt', ['', ' ', 'test robot type'])
+    @pytest.mark.parametrize('ri', ['', ' ', 'test_robot_id'])
+    @pytest.mark.parametrize('rt', ['', ' ', 'test_robot_type'])
     @pytest.mark.parametrize('v', [None, '', ' ', 'test value', '   test value 2   '])
-    def test_success_env(self, monkeypatch, endpoint, mocked_post, fs, fsp, ri, rt, v):
+    def test_success_env(self, monkeypatch, endpoint, mocked_patch, fs, fsp, ri, rt, v):
         os.environ[const.FIWARE_SERVICE] = fs
         os.environ[const.FIWARE_SERVICEPATH] = fsp
         os.environ[const.ROBOT_ID] = ri
         os.environ[const.ROBOT_TYPE] = rt
 
-        mocked_post(monkeypatch, endpoint, fs, fsp, ri, rt, v)
+        mocked_patch(monkeypatch, endpoint, fs, fsp, ri, rt, v)
         send_request_to_orion(endpoint, v)
 
     def test_raise_error(self, monkeypatch, endpoint):
-        def mocked_post(endpoint, headers, data):
+        def mocked_patch(endpoint, headers, data):
             raise requests.exceptions.RequestException()
 
-        monkeypatch.setattr(requests, 'post', mocked_post)
+        monkeypatch.setattr(requests, 'patch', mocked_patch)
         with pytest.raises(requests.exceptions.RequestException):
             send_request_to_orion(endpoint, 'dummy')
